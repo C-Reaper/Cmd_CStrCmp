252870bc345bbc44458461bab28622e75555c004
codeleaded <gnu.c.reaper@gmail.com>
2026-01-29 00:03:33 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -1,8 +1,9 @@
 #include "/home/codeleaded/System/Static/Library/CStrCmp.h"
 
 int main(int argc,char **argv){
-    CStrCmp cc = CStrCmp_New("Hello World","Hello Welt");
-    CStrCmp_Build(&cc);
+    CStrCmp cc = CStrCmp_Load("./data/Test0.txt","./data/Test1.txt");
+    CStrCmp_Difference(&cc);
+    //CStrCmp_Build(&cc);
     CStrCmp_Print(&cc);
     CStrCmp_Free(&cc);
     return 0;
