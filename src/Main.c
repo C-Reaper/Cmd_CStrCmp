54d7c61aefdee8e59d670ab0ca8892a7c891151d
codeleaded <gnu.c.reaper@gmail.com>
2026-01-28 21:22:25 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -1,19 +1,8 @@
-#include "/home/codeleaded/System/Static/Library/VCtl.h"
+#include "/home/codeleaded/System/Static/Library/CStrCmp.h"
 
 int main(int argc,char **argv){
-    VCtl vc = VCtl_New("vc","vc/.vcignore");
-    
-    //VCtl_Init(&vc);
-    //Files_Create("vc/src/Header0.h");
-    //VCtl_MakeBranch(&vc,"root","main","main got added!");
-    //Files_Create("vc/src/Header1.h");
-    //VCtl_UpdateBranch(&vc,"main","main got updated!");
-    //VCtl_WriteBranches(&vc);
-
-    VCtl_ReadBranches(&vc);
-    VCtl_BackBranch(&vc,"main");
-    
-    VCtl_Print(&vc);
-    VCtl_Free(&vc);
+    CStrCmp cc = CStrCmp_New("Hello World","Hello Welt");
+    CStrCmp_Print(&cc);
+    CStrCmp_Free(&cc);
     return 0;
 }
