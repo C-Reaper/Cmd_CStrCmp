b564923e33bf58fe15f2e42dc2b74e295e48b178
codeleaded <gnu.c.reaper@gmail.com>
2026-01-30 17:11:35 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -2,9 +2,19 @@
 
 int main(int argc,char **argv){
     CStrCmp cc = CStrCmp_Load("./data/Test0.txt","./data/Test1.txt");
+    
     CStrCmp_Difference(&cc);
-    //CStrCmp_Build(&cc);
     CStrCmp_Print(&cc);
+    CStrCmp_DiffMap_WriteF(&cc.ccdm,"./data/Diff");
+    
+    //CStrCmp_DiffMap_Add(&cc.ccdm,&cc.src);
+    //Vec_CStr_Print(&cc.src);
+
+    //CStrCmp_DiffMap ccdm = CStrCmp_DiffMap_ReadF("./data/Diff");
+    //CStrCmp_DiffMap_Add(&ccdm,&cc.src);
+    //Vec_CStr_Print(&cc.src);
+    //CStrCmp_DiffMap_Free(&ccdm);
+
     CStrCmp_Free(&cc);
     return 0;
 }
