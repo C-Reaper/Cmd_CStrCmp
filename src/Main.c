cfb6dacd5cd3eb23551621adb5c04e37b33c16df
codeleaded <gnu.c.reaper@gmail.com>
2026-02-01 02:14:07 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -1,12 +1,43 @@
 #include "/home/codeleaded/System/Static/Library/CStrCmp.h"
 
 int main(int argc,char **argv){
-    CStrCmp cc = CStrCmp_Load("./data/Test0.txt","./data/Test1.txt");
+    CStrCmp cc0 = CStrCmp_Load("./data/Test0.txt","./data/Test1.txt");
+    CStrCmp_Difference(&cc0);
+    CStrCmp_Print(&cc0);
+    CStrCmp_DiffMap_WriteF(&cc0.ccdm,"./data/Diff0");
+
+    CStrCmp cc1 = CStrCmp_Load("./data/Test0.txt","./data/Test2.txt");
+    CStrCmp_Difference(&cc1);
+    CStrCmp_Print(&cc1);
+    CStrCmp_DiffMap_WriteF(&cc1.ccdm,"./data/Diff1");
     
-    CStrCmp_Difference(&cc);
-    CStrCmp_Print(&cc);
-    CStrCmp_DiffMap_WriteF(&cc.ccdm,"./data/Diff");
+    CStrCmp_DiffMap mg = CStrCmp_DiffMap_Merge(&cc0.ccdm,&cc1.ccdm);
+    CStrCmp_DiffMap_Add(&mg,&cc0.src);
+    CStrCmp_DiffMap_Free(&mg);
+
+    Vec_CStr_Print(&cc0.src);
+
+    CStrCmp_Free(&cc1);
+    CStrCmp_Free(&cc0);
+
+
+
+    CStrCmp cc02 = CStrCmp_Load("./data/Test0.txt","./data/Test1.txt");
+    CStrCmp_Difference(&cc02);
+    CStrCmp_Print(&cc02);
+    CStrCmp_DiffMap_WriteF(&cc02.ccdm,"./data/Diff02");
+
+    CStrCmp cc12 = CStrCmp_Load("./data/Test1.txt","./data/Test2.txt");
+    CStrCmp_Difference(&cc12);
+    CStrCmp_Print(&cc12);
+    CStrCmp_DiffMap_WriteF(&cc12.ccdm,"./data/Diff12");
     
+    CStrCmp_DiffMap_Acc(&cc02,&cc12);
+    CStrCmp_DiffMap_Print(&cc02);
+
+    CStrCmp_Free(&cc12);
+    CStrCmp_Free(&cc02);
+
     //CStrCmp_DiffMap_Add(&cc.ccdm,&cc.src);
     //Vec_CStr_Print(&cc.src);
 
@@ -14,7 +45,5 @@ int main(int argc,char **argv){
     //CStrCmp_DiffMap_Add(&ccdm,&cc.src);
     //Vec_CStr_Print(&cc.src);
     //CStrCmp_DiffMap_Free(&ccdm);
-
-    CStrCmp_Free(&cc);
     return 0;
 }
